06c35020705ccc4de9d388e3ba4faf904059c84c
chigaevi <chigaev.i@gmail.com>
2023-01-13 09:32:53 +0700

    Commit 4. optimization

--- main.py
+++ main.py
@@ -4,49 +4,46 @@ import re
 
 def distrib_name():
     i = 0
-    for staff in contacts_list:
-      if staff[1] == '' and staff[2] == '':
-          name_staff = staff[0].split()
-          for ind in range(len(name_staff)):
-              contacts_list[i][ind] = name_staff[ind]
+    for worker in contacts_list:
+      if worker[1] == '' and worker[2] == '':
+          name_worker = worker[0].split()
+          for ind in range(len(name_worker)):
+              contacts_list[i][ind] = name_worker[ind]
           i += 1
-          # print(name_staff)
-      elif staff[1] != '' and staff[2] == '':
-          name_staff = [staff[0]]
-          name_staff.extend(staff[1].split())
-          for ind in range(len(name_staff)):
-              contacts_list[i][ind] = name_staff[ind]
+      elif worker[1] != '' and worker[2] == '':
+          name_worker = [worker[0]]
+          name_worker.extend(worker[1].split())
+          for ind in range(len(name_worker)):
+              contacts_list[i][ind] = name_worker[ind]
           i += 1
-          # print(name_staff)
       else:
           i += 1
+    return contacts_list
+
 
 def edit_phone_number():
     i = 0
-    for staff in contacts_list:
-        phone_number = staff[5]
+    for worker in contacts_list:
+        phone_number = worker[5]
         pattern = r'(\+7|^8)\s*\(*(\d{3})\)*\s*\-*(\d{3})\s*\-*(\d{2})\s*\-*(\d{2})\s*\(*(доб.)*\s*(\d{4})*\)*'
         rep = r'+7(\2)\3-\4-\5 \6\7'
         new_number = re.sub(pattern,rep,phone_number)
         contacts_list[i][5] = new_number
         i += 1
+    return contacts_list
 
 def join_records():
     i = 0
-    count_staff = len(contacts_list)
-    count_prop = len(contacts_list[0])
-    for staff in contacts_list:
-        if i+1 == count_staff:
-            continue
-        lastname = staff[0]
-        for ind in range(i, count_staff):
+    for worker in contacts_list:
+        lastname = worker[0]
+        for ind in range(i, len(contacts_list)):
              if lastname == contacts_list[ind][0]:
-                for ind2 in range(count_prop):
-                    if contacts_list[ind][ind2] == '' or staff[ind2] == '':
-                        contacts_list[i][ind2] = contacts_list[ind][ind2] + staff[ind2]
-                        contacts_list[ind][ind2] = contacts_list[i][ind2]
+                for ind2 in range(len(contacts_list[0])):
+                    if contacts_list[ind][ind2] == '' or worker[ind2] == '':
+                        contacts_list[i][ind2] = contacts_list[ind][ind2] + worker[ind2]
+                contacts_list[ind] = contacts_list[i]
         i += 1
-
+    # collecting a new list without duplicates:
     new_contacts_list = []
     for i in contacts_list:
         if i in new_contacts_list:
